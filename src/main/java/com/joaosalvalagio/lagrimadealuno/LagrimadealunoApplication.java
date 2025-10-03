08b8db795f95be918e55263635a62279168c0720
João Miguel Silva Salvalagio <joaosalvalagio@gmail.com>
2025-10-02 22:13:04 -0300

    API

---
+++ src/main/java/com/joaosalvalagio/lagrimadealuno/LagrimadealunoApplication.java
@@ -0,0 +1,13 @@
+package com.joaosalvalagio.lagrimadealuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagrimadealunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagrimadealunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaosalvalagio/lagrimadealuno/LagrimadealunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaosalvalagio.lagrimadealuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagrimadealunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
