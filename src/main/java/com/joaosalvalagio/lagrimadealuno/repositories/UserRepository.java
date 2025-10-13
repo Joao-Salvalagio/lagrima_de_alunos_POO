b6f103251b7d563586c4f5f9ed3f9a67695da5ea
João Miguel Silva Salvalagio <joaosalvalagio@gmail.com>
2025-10-13 20:20:59 -0300

    Att 13/10

--- src/main/java/com/joaosalvalagio/lagrimadealuno/repositories/UserRepository.java
+++ src/main/java/com/joaosalvalagio/lagrimadealuno/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.joaosalvalagio.lagrimadealuno.repositories;
 
 import com.joaosalvalagio.lagrimadealuno.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
+    UserDetails findbyLogin(String login);
 }
--- src/main/java/com/joaosalvalagio/lagrimadealuno/services/UserService.java
+++ src/main/java/com/joaosalvalagio/lagrimadealuno/services/UserService.java
@@ -2,10 +2,18 @@ package com.joaosalvalagio.lagrimadealuno.services;
 
 import com.joaosalvalagio.lagrimadealuno.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        return userRepository.findbyLogin(username);
+    }
 }
